f782a0fe070d39acb5c18716dd7d3b8ffd25bb5d
Diadlo <polsha3@gmail.com>
2015-01-01 18:34:40 +0300

    Сделал подключение getch() для обеих систем

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#ifdef __linux__ 
+	#include "conio_l.cpp"
+#elif _WIN32
+	#include <conio.h>
+#endif
+
+using namespace std;
+
 int main()
 {
+	int a = getch();
+	cout << a << endl;
 	return 0;
 }
