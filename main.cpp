9d616ba3ae4e2d2db8d7815b42a14ec5adc4b3e4
Nepfion <nepfion96@gmail.com>
2015-01-01 18:37:24 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
-int main()
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int const n = 2;
+int const m = 3;
+void printLocation(char a[m][n])
 {
-	return 0;
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+      cout << a[i][j];
+    cout << "\n";
+  }      
+  return;
+}
+int main ()
+{
+  char a[m][n] = {{'#'}};
+  printLocation(a);
+  system("Pause");
+  return 0;
 }
