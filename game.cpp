a34600cafb13f584d505eb53d68faadb4b1eacac
Diadlo <polsha3@gmail.com>
2015-01-09 01:25:20 +0300

    Сделал загрузку сандартной "кампании"

---
+++ game.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "map.h"
+#include "init.h"
+#include "move.h"
+#ifdef __linux__
+	#define WINDOWS 0
+	#include "conio.cpp"
+#elif _WIN32
+	#define WINDOWS 1
+	#include <conio.h>
+#endif
+
+using namespace std;
+
+int const ESC = 27;
+char *door;
+
+int game (char** map)
+{
+	//char** map = getMap();
+	position player, door = getPositionDoor(map);
+	printLocation(map);
+	bool quit = false;
+	while (!quit)
+	{
+		int key = getch();
+		if (key == ESC)
+			cout << "menu(true)";
+			//menu(true);
+		move(map, key);
+		player = getPositionPlayer(map);
+		if ((player.i == door.i) && (player.j == door.j))
+			return 1;
+	}
+	getch();
+	return -1;
+}
