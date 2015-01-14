f527d80d1a97938045a981b77e1a55fe42ae6302
Diadlo <polsha3@gmail.com>
2015-01-15 00:35:29 +0300

    Рефакторинг

--- map.cpp
+++ map.cpp
@@ -7,57 +7,83 @@
 #include <fstream>
 #include "./map.h"
 #include "./clean.h"
+#ifdef __linux__
+	#include "multiplatform.h"
+#endif
 
 int N, M;
 
 std::string readPath();
+void printError();
+char** newArray(int, int);
+void deleteArray(char**, int);
+void copyArray(char** from, char** to, int M, int N);
+void copyLine(const char* from, char* to, int N);
 
 char** readMap(std::string name) {
-  #ifdef _WIN32
-    setlocale(0, "");
-  #endif
-  M = 0;
-  std::ifstream file(name.c_str());
-  std::string line;
-  getline(file, line);
-  N = line.length();
-  char** map = new char*[++M];
-  map[0] = new char[N];
-  for (int i = 0; i < N; i++)
-    map[0][i] = line[i];
-
-  while (getline(file, line)) {
-    char** old_map = new char*[M];
-    for (int i = 0; i < M; i++)
-      old_map[i] = new char[N];
-
-    for (int i = 0; i < M; i++)
-      for (int j = 0; j < N; j++)
-        old_map[i][j] = map[i][j];
-
-    for (int i = 0; i < M; i++)
-      delete []map[i];
-
-    map = new char*[++M];
-    for (int i = 0; i < M; i++)
-      map[i] = new char[N];
-
-    for (int i = 0; i < M-1; i++)
-      for (int j = 0; j < N; j++)
-        map[i][j] = old_map[i][j];
-
-    for (int i = 0; i < N; i++)
-      map[M-1][i] = line[i];
-  }
-  return map;
+	setlocale(0, "");
+	std::ifstream file(name.c_str());
+	std::string line;
+	getline(file, line);
+	N = line.length();
+	M = 0;
+	
+	char** map = newArray(++M, N);
+	copyLine(line.c_str(), map[0], N);
+	
+	while (getline(file, line)) {
+		char** oldMap = newArray(M,N);
+		copyArray(map, oldMap, M, N);
+		deleteArray(map, M);
+		map = newArray(++M, N);
+		copyArray(oldMap, map, M-1, N);
+		copyLine(line.c_str(), map[M-1], N);
+	}
+	return map;
 }
 
+char** newArray(int M, int N) {
+	char** a = new char*[M];
+	for (int i = 0; i < M; i++)
+		a[i] = new char[N];
+	return a;
+}
 
+void deleteArray(char** a, int N) {
+	for (int i = 0; i < N; i++)
+		delete []a[i];
+}
+
+void copyLine(const char* from, char* to, int N) {
+	for (int i = 0; i < N; i++)
+		to[i] = from[i];
+}
+
+void copyArray(char** from, char** to, int M, int N) {
+	for (int i = 0; i < M; i++)
+		for (int j = 0; j < N; j++)
+			to[i][j] = from[i][j];
+}
 
 char** getMap() {
-  std::string name = readPath();
-  char** map = readMap(name);
-  return map;
+	do {
+		std::string path = readPath();
+		std::ifstream file(path.c_str());
+		if (file.good()){
+			char** map = readMap(path);
+			return map;
+		}
+		else
+			printError();
+	} while(true);
+}
+
+void printError() {
+	#ifdef __linux__
+		std::cout << "Ð¢Ð°ÐºÐ¾Ð³Ð¾ Ñ„Ð°Ð¹Ð»Ð° Ð½Ðµ ÑÑƒÑ‰ÐµÑÑ‚Ð²ÑƒÐµÑ‚.\n";
+	#elif WIN32
+		std::cout << "?????? ????? ?? ??????????.\n";
+	#endif
 }
 
 void printMap(char** map) {
@@ -74,7 +100,7 @@ std::string readPath() {
   #ifdef __linux__
     std::cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð¿ÑƒÑ‚ÑŒ Ðº Ñ„Ð°Ð¹Ð»Ñƒ ÑÐ¾Ð´ÐµÑ€Ð¶Ð°Ñ‰ÐµÐ¼Ñƒ ÐºÐ°Ñ€Ñ‚Ñƒ: ";
   #elif _WIN32
-    std::cout << "Ââåäèòå ïóòü ê ôàéëó, ñîäåðæàùåìó êàðòó: ";
+    std::cout << "??????? ???? ? ?????, ??????????? ?????: ";
   #endif
   std::string name;
   std::cin >> name;
--- start.cpp
+++ start.cpp
@@ -20,31 +20,50 @@ const int N_maps = 3;
 
 void printWinMessage();
 void pressAnyKey();
+std::string askName();
+void writeRecords(std::string, int);
+void lastMap(int time);
 
 int start() {
-  #ifdef _WIN32
-    setlocale(0, "");
-  #endif
-  double time = 0;
-  std::string maps[N_maps] = {"1", "2", "3"};
-  for (int i = 0; i < N_maps; i++) {
-    char** map = readMap("standart/" + maps[i] + ".map");
-	int res = game(map);
-    if (res != 'q') {
-      time += res;
-      if (i == N_maps - 1) {
-        printWinMessage();
-        std::string name;
-        std::cin >> name;
-        std::stringstream text;
-        text << name << " " << time;
-        writeRecords(text.str());
-        pressAnyKey();
-        return 1;
-      }
-    }
-  }
-  return 0;
+	setlocale(0, "");
+	double time = 0;
+	std::string maps[N_maps] = {"1", "2", "3"};
+	for (int i = 0; i < N_maps; i++) {
+		char** map = readMap("standart/" + maps[i] + ".map");
+		int res = game(map);
+		if (res == 'q')
+			return 'q';
+		else {
+			time += res;
+			if (i == N_maps - 1)
+				lastMap(time);
+		}
+	}
+  return 1;
+}
+
+void lastMap(int time) {
+	printWinMessage();
+	std::string name = askName();
+	writeRecords(name, time);
+	pressAnyKey();
+}
+
+void writeRecords(std::string name, int time) {
+	std::stringstream text;
+	text << name << " " << time;
+	writeRecords(text.str());
+}
+
+std::string askName() {
+	#ifdef __linux__
+		std::cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð²Ð°ÑˆÐµ Ð¸Ð¼Ñ: ";
+	#elif WIN32
+		std::cout << "Ââåäèòå âàøå èìÿ: ";
+	#endif
+	std::string name;
+	std::cin >> name;
+	return name;
 }
 
 void pressAnyKey() {
@@ -61,10 +80,8 @@ void pressAnyKey() {
 
 void printWinMessage() {
   #ifdef __linux__
-    std::cout << "\nÐ’Ñ‹ Ð¿Ñ€Ð¾ÑˆÐ»Ð¸ Ð²ÑÐµ ÑƒÑ€Ð¾Ð²Ð½Ð¸. ÐŸÐ¾Ð·Ð´Ñ€Ð°Ð²Ð»ÑÐµÐ¼!\n"
-              << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð²Ð°ÑˆÐµ Ð¸Ð¼Ñ: ";
+    std::cout << "\nÐ’Ñ‹ Ð¿Ñ€Ð¾ÑˆÐ»Ð¸ Ð²ÑÐµ ÑƒÑ€Ð¾Ð²Ð½Ð¸. ÐŸÐ¾Ð·Ð´Ñ€Ð°Ð²Ð»ÑÐµÐ¼!\n";
   #elif _WIN32
-	std::cout << "\nÂû ïðîøëè âñå óðîâíè. Ïîçäðàâëÿåì!\n"
-		<< "Ââåäèòå âàøå èìÿ: ";
+	std::cout << "\nÂû ïðîøëè âñå óðîâíè. Ïîçäðàâëÿåì!\n";
   #endif
 }
