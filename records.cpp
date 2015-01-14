76fb9691ea45472d24b0f41fbb44595147fd3600
Nepfion <nepfion96@gmail.com>
2015-01-14 12:59:53 +0300

    Добавил считывание считывание в память файл records.txt, но пока что не реализовал перезапись рекордов

--- records.cpp
+++ records.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <map>
 #include "clean.h"
 #ifdef __linux__
   #define WINDOWS 0
@@ -12,9 +13,30 @@
 #include <conio.h>
 #endif
 
+int findName(std::string line);
+
 void writeRecords(std::string text) {
-  std::ofstream file("records.txt", std::ios_base::app);
-  file << text << "\n";
+  std::ifstream file("records.txt");
+  std::string line;
+  std::map <std::string, int> record;
+  record[text.substr(0, text.rfind(' '))] = strtol((text.substr(text.rfind(' ')+1, '\n')).c_str(), NULL, 10);
+  bool flag = false;
+  while (getline(file, line)) {
+    record[line.substr(0, line.rfind(' '))] = strtol((line.substr(line.rfind(' ')+1, '\n')).c_str(), NULL, 10);
+    if (line.substr(0, line.rfind(' ')) == text.substr(0, text.rfind(' ')))
+      flag = true;
+    }
+  if (!flag) {
+    std::ofstream file("records.txt", std::ios_base::app);
+    file << text << "\n";
+  }
+  else
+    if (record[text.substr(0, text.rfind(' '))] > strtol((text.substr(text.rfind(' ')+1, '\n')).c_str(), NULL, 10)) {
+      record[text.substr(0, text.rfind(' '))] = strtol((text.substr(text.rfind(' ')+1, '\n')).c_str(), NULL, 10);
+      std::ofstream file("records.txt", std::ios_base::app);
+      file << text << "\n";
+    }
+   return;         
 }
 
 int readRecords() {
